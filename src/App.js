3b590f0f1468c4defc9fc5c9bdfe19d5bf22c1cd
Sol-Devloper <blockhunt14@gmail.com>
2023-09-27 23:54:17 +1300

    React form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Home from "./pages/Home";
-import Menu from "./pages/Menu";
+import Menu from "./pages/Home";
 import Footer from "./components/Footer";
 import Reservations from "./pages/Reservations";
 import AboutUs from "./pages/AboutUs";
@@ -13,13 +13,11 @@ function App() {
       <Header/>
       <nav className="container">
         <Link to='/' className="containerLink">Home</Link>
-        <Link to='/menu' className="containerLink" >Menu</Link>
         <Link to='/reservations' className="containerLink">Reservations</Link>
         <Link to='/about-us' className="containerLink">About Us</Link>
       </nav>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/menu' element={<Menu />}/>
         <Route path='/reservations' element={<Reservations />}/>
         <Route path='/about-us' element={<AboutUs />}/>
       </Routes>
--- src/pages/Reservations.js
+++ src/pages/Reservations.js
@@ -1,8 +1,59 @@
+import { useState } from 'react';
+import '../App.css';
+
 export default function Reservations() {
+  const [name, setName] = useState('');
+  const [date, setDate] = useState();
+  const [numberOfGuests, setNumberOfGuests] = useState();
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  }
+
+  const handleDate = (e) => {
+    setDate(e.target.value);
+  }
+
+  const handleGuests = (e) => {
+    setNumberOfGuests(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('Your table has been booked!!')
+  }
+
     return (
-      <>
-        <p>Home is work in progress</p>
-      </>
+      <div>
+        <form className='form' onSubmit={handleSubmit}>
+          <label className='formLabel'  htmlFor='res-name'> Name: 
+            <input type='text' id='res-name' onChange={handleName}/>
+          </label>
+          <label className='formLabel' htmlFor='res-date'>Choose date: 
+            <input type='date' id='res-date'onChange={handleDate} />
+          </label>
+          <label className='formLabel' htmlFor='res-time'>Choose time:
+            <select>
+              <option>17:00</option>
+              <option>18:00</option>
+              <option>19:00</option>
+              <option>20:00</option>
+              <option>21:00</option>
+              <option>22:00</option>
+            </select>
+          </label>
+          <label className='formLabel' htmlFor='guests'>Number of guests:
+            <input type='number' placeholder='1' min='1' max='10' id='guests' onChange={handleGuests} />
+          </label>
+          <label className='formLabel' htmlFor='occasion'>Occasion:
+            <select id='occasion'>
+              <option>Birthday</option>
+              <option>Anniversary</option>
+            </select>
+          </label>
+          <input className='formLabel' type='submit' value='Make Your reservation' />
+        </form>
+      </div>
     );
   }
   
