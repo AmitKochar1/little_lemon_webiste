e0331e91aa92e35bbd322826a79f3a82a0904d82
Sol-Devloper <blockhunt14@gmail.com>
2023-10-18 23:53:53 +1300

    update in form and useReducer Hook

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,125 +1,147 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../App.css';
 
+export default function BookingForm(availableTimes, dispatch, submitForm) {
+  console.log(dispatch);
+  const [formData, setFormData] = useState({
+    fullName: '',
+    number: "",
+    date: '',
+    time: '00:00',
+    noOfGuests: 1,
+    occasion: 'birthday'
+  })
 
-export default function BookingForm(props) {
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [tel, setTel] = useState('');
-  const [date, setDate] = useState();
-  const [people, setPeople] = useState('0');
-  const [occasion, setOccasion] = useState('');
-  const [comments, setComments] = useState('')
-  const [finalTime, setFinalTime] = useState( ''
-    // props.availableTimes.map((times) => <option> {times} </option>)
-  )
-
-  const handleName = (e) => {
-    setFullName(e.target.value);
+  const handleFormChange = (e) => {
+    const {name, value} = e.target
+    setFormData((pervFormData) => ({
+      ...pervFormData,
+      [name]: value
+    }))
   }
 
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-  }
-
-  const handleTel = (e) => {
-    setTel(e.target.value);
-  }
-
-  const handlePeople = (e) => {
-    setPeople(e.target.value);
-  }
-
-  const handleOcassion = (e) => {
-    setOccasion(e.target.value);
-  }
-
-  const handleComments = (e) => {
-    setComments(e.target.value);
-  }
-
-  function handleDateChange(e){
-    setDate(e.target.value);
-
-    var dateString = e.target.value;
-    const date = new Date(dateString);
-
-    props.updateTimes(date);
-    
-    setFinalTime('')
-
-    // setFinalTime(props.availableTimes.map((times) => <option>{times}</option>))
+  const handleDateChange = async(e) => {
+    const {name, value} = e.target
+    setFormData((pervFormData) => ({
+      ...pervFormData,
+      [name]: value
+    }))
+    dispatch({type: "UPDATE_TIMES", payload: value})
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert('Your table has been booked!!');
-    setFullName("");
-    setPeople("0");
+    submitForm(formData)
   }
+
+  const currentDate = new Date().toString().split('T')[0];
+  const options = availableTimes.map(time => <option key={time}>{time}</option>)
+
+  // const [fullName, setFullName] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [tel, setTel] = useState('');
+  // const [date, setDate] = useState();
+  // const [people, setPeople] = useState('0');
+  // const [occasion, setOccasion] = useState('');
+  // const [comments, setComments] = useState('')
+  // const [finalTime, setFinalTime] = useState( ''
+  //   // props.availableTimes.map((times) => <option> {times} </option>)
+  // )
+
+  // const handleName = (e) => {
+  //   setFullName(e.target.value);
+  // }
+
+  // const handleEmail = (e) => {
+  //   setEmail(e.target.value);
+  // }
+
+  // const handleTel = (e) => {
+  //   setTel(e.target.value);
+  // }
+
+  // const handlePeople = (e) => {
+  //   setPeople(e.target.value);
+  // }
+
+  // const handleOcassion = (e) => {
+  //   setOccasion(e.target.value);
+  // }
+
+  // const handleComments = (e) => {
+  //   setComments(e.target.value);
+  // }
+
+  // function handleDateChange(e){
+  //   setDate(e.target.value);
+
+  //   var dateString = e.target.value;
+  //   const date = new Date(dateString);
+
+  //   props.updateTimes(date);
+    
+  //   setFinalTime('')
+
+  //   // setFinalTime(props.availableTimes.map((times) => <option>{times}</option>))
+  // }
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   alert('Your table has been booked!!');
+  //   setFullName("");
+  //   setPeople("0");
+  // }
     return (
-      <>
-      <h1>Book a Table</h1>
-        <form className='form' onSubmit={handleSubmit} >
-          <div className='formLabel'>
-            <label htmlFor='fullName'> Full Name<br />
-              <input type='text' id='fullName' onChange={handleName} value={fullName} placeholder='Full Name'/>
-            </label>
-          </div>
+      <main>
+        <h1>Please, fill in the form below to book your table </h1>
+          <form className='form' onSubmit={handleSubmit} >
+            <div className='formLabel'>
+              <label htmlFor='fullName'> Full Name </label>
+                <input type='text' id='fullName' onChange={handleFormChange} value={formData.fullName} placeholder='Full Name'/>
+            </div>
 
-          <div className='formLabel'>
-            <label htmlFor='emailAdd'> Email Address<br />
-              <input type='email' id='emailAdd' onChange={handleEmail} value={email} placeholder='Email Address'/>
-            </label>
-          </div>
+            <div className='formLabel'>
+              <label htmlFor='teleNumber'>Phone Number </label>
+                <input type='phone' id='teleNumber' onChange={handleFormChange} value={formData.number} placeholder='TelePhone Number'/>
+            </div>
 
-          <div className='formLabel'>
-            <label htmlFor='teleNumber'>Phone Number <br />
-              <input type='phone' id='teleNumber' onChange={handleTel} value={tel} placeholder='TelePhone Number'/>
-            </label>
-          </div>
+            <div className='formLabel'>
+              <label className='formLabel' htmlFor='date'>Choose date: </label>
+                <input type='date' id='date'onChange={handleDateChange} value={formData.date} required min={currentDate}/>
+            </div>
 
-          <div className='formLabel'>
-            <label className='formLabel' htmlFor='guests'>Number of guests:<br />
-              <input type='number' id='guests' placeholder='1' min='1' max='10' value={people} onChange={handlePeople} />
-            </label>
-          </div>
-
-          <div className='formLabel'>
-            <label className='formLabel' htmlFor='date'>Choose date:<br />
-              <input type='date' id='date'onChange={handleDateChange} value={date} />
-            </label>
-          </div>
-
-          <div className='formLabel'>
-            <label className='formLabel' htmlFor='time'>Choose time:<br />
-            <select id='time' required>
-              {finalTime}
-            </select>
-            </label>
-          </div>
-          
-          <div className='formLabel'>
-            <label className='formLabel' htmlFor='occasion'>Occasion:<br />
-              <select id='occasion' value={occasion} onChange={handleOcassion}>
-                <option>None</option>
-                <option>Birthday</option>
-                <option>Anniversary</option>
-                <option>Engagement</option>
-                <option>other</option>
+            <div className='formLabel'>
+              <label className='formLabel' htmlFor='time'>Choose time: </label>
+              <select id='time' value={formData.time} onChange={handleFormChange} required>
+                {options}
               </select>
-            </label>
-          </div>
+            </div>
+            
+            <div className='formLabel'>
+              <label className='formLabel' htmlFor='occasion'>Occasion: </label>
+                <select id='occasion' value={formData.occasion} onChange={handleFormChange}>
+                  <option>Birthday</option>
+                  <option>Anniversary</option>
+                  <option>Engagement</option>
+                  <option>other</option>
+                </select>
+            </div>
 
-          <div className='formLabel'>
-            <label htmlFor='comments'>Additonal Comments:<br />
-            <textarea id='comments' placeholder='Additonal Comments' value={comments} onChange={handleComments}/>            
-            </label>
-          </div>
-          
-          <button disabled={!fullName} type='submit'>Book Your Table</button>
-        </form>
-        </>
+            <div className='formLabel'>
+              <label className='formLabel' htmlFor='guests'>Number of guests: </label>
+                <input type='number' id='guests' placeholder='1' min='1' max='10' required value={formData.noOfGuests} onChange={handleFormChange} />
+            </div>
+
+            {/* <div className='formLabel'>
+              <label htmlFor='comments'>Additonal Comments:<br />
+              <textarea id='comments' placeholder='Additonal Comments' value={comments} onChange={handleComments}/>            
+              </label>
+            </div> */}
+            
+            <button disabled={!formData.fullName} type='submit'>Book Your Table</button>
+          </form>
+        </main>
 )}
 
   
--- src/pages/Reservation.js
+++ src/pages/Reservation.js
@@ -1,24 +1,47 @@
 import BookingForm from "../components/BookingForm";
-import Navbar from "../components/Nav";
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
+import { fetchAPI, submitAPI } from "../api";
+import { useNavigate } from "react-router-dom";
 
 export default function Reservation(){
 
-    function updateTimes(date) {
-        return fetchAPI(date);
-      }
-    
-    const initializeTimes = fetchAPI(new Date());
-    
-    //   const [availableTimes, dispatch] = useReducer(updateTimes, output);
+    const [date, setDate] = useState(new Date());
 
-    const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes);
+    function initializeTimes(date) {
+        return fetchAPI(date)
+    }
+
+    function updateTimes(date) {
+        const dateObj = new Date(date);
+        return fetchAPI(dateObj);
+    }
+
+    const navigate = useNavigate();
+    
+    function submitForm(formData){
+        const isSubmitted = submitAPI(formData);
+
+        if(isSubmitted){
+            navigate('/')
+        }
+    }
+
+    function reducer(state, action) {
+        let newState;
+        switch(action.type){
+            case 'UPDATE_TIMES':
+                const newDate = new Date(action.payload);
+                newState = updateTimes(newDate);
+                break;
+            default:
+                throw new Error()
+        }
+        return newState;
+    }
+    
+    const [availableTimes, dispatch] = useReducer(reducer, initializeTimes(date));
     
     return(
-        <>
-        <Navbar/>
-        <BookingForm availableTimes={availableTimes} updateTimes={dispatch} />
-        </>
-        
+        <BookingForm availableTimes={availableTimes} dispatch={dispatch} submitForm={submitForm} />        
     )
 }
